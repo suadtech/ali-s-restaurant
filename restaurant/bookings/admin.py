6edc8ad71d6d8e5a45d2e6b7efd297880c42c780
suad alhodifi <suadtech1@gmail.com>
2025-03-29 19:19:50 +0000

    create forms.py file

--- restaurant/bookings/admin.py
+++ restaurant/bookings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Table, Booking
 # Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number', 'capacity', 'location', 'is_active')
+    list_filter = ('capacity', 'is_active')
+    search_fields = ('number', 'location')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'table', 'date', 'time', 'guests', 'is_cancelled')
+    list_filter = ('date', 'is_cancelled')
+    search_fields = ('user__username', 'table__number')
+    date_hierarchy = 'date'
--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
